dcdf6b7918f517a7e5f1d3786ffaf277e5892728
Dawid Nowak <code@dnowak.pl>
2014-07-04 19:27:17 +0200

    error action resulting in blank page response

--- web/index.php
+++ web/index.php
@@ -2,12 +2,29 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+class SomeClassWithError
+{
+    public function runMethodWithError()
+    {
+        $this->nonExistingMethod();
+    }
+}
+
 $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
 
-$app = new Silex\Application();
+$debug = 'true' === $request->query->get('debug');
 
-$app->get('/', function () {
-    return 'Hello World!';
+$app = new Silex\Application(['debug' => $debug]);
+
+$app->get('/', function (Silex\Application $app) {
+    return sprintf('Hello World! (debug %s)', $app['debug'] ? 'ON' : 'OFF');
+});
+
+$app->get('/error', function () {
+    $object = new SomeClassWithError();
+    $object->runMethodWithError();
+
+    return 'Should not get to that…';
 });
 
 $app->run($request);
